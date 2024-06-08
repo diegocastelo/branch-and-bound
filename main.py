831c4065af3a4df812a016f0eb5049e21bd14736
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-06-08 14:51:27 -0300

    :sparkles: initial solve

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+from machine import Machine
 
+def read_data(filename):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    first_line = lines[0].split()
+    num_variables = int(first_line[0])
+    num_conditions = int(first_line[1])
+
+    objective = list(map(int, lines[1].split()))
+    constraints = []
+    for i in range(2, 2 + num_conditions):
+        constraints.append(list(map(int, lines[i].split())))
+
+    print("Objective:", objective)
+    print("Variables:", num_variables)
+    print("Constraints:", constraints)
+
+    return objective, num_variables, constraints
+
+
+if __name__ == '__main__':
+    objective, num_variables, constraints = read_data("input.txt")
+
+    machine = Machine(objective, num_variables, constraints)
+    solution = machine.solve()
+
+    if solution:
+        print("Solução ótima encontrada:")
+        for i, value in enumerate(solution):
+            print(f'x{i+1} = {value}')
+    else:
+        print("Nenhuma solução ótima encontrada.")
---
+++ node.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, status=None, indexes=None, sign=None, value=None, solution=None, cost=None):
+        self.status = status
+        self.indexes = indexes
+        self.sign = sign
+        self.value = value
+        self.solution = solution
+        self.cost = cost
